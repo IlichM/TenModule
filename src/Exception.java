427220fb329628c808fe807601d1a4bcb029ec6f
IlichM <viruslm@mail.ru>
2021-05-29 14:54:33 +0300

    1 commit

---
+++ src/Exception.java
@@ -0,0 +1,31 @@
+import  java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+
+public class Exception {
+    public static void main(String[] args) {
+//        try {
+//            NumberFormat nf = NumberFormat.getInstance();
+//            //специально создаем ситуацию, которая приведет к исключению
+//            System.out.println(nf.parse("NOT A NUMBER"));
+//        }
+//        catch (ParseException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        System.out.println("Конец программы!");
+
+        String string = "123";
+        try
+        {
+            char chr = string.charAt(10);
+        }
+        catch(StringIndexOutOfBoundsException ex)
+        {
+            System.out.println(ex.toString());
+        }
+        System.out.println("Конец программы!");
+
+
+    }
+}
---
+++ src/IndexOfBoundsException.java
@@ -0,0 +1,10 @@
+public class IndexOfBoundsException {
+    public static void main(String[] args) {
+        int[] array = {1,2,-1,5,3};
+        int s = 0, i;
+        for(i = 0; i < array.length; i++) {
+            s+=array[i];
+        }
+        System.out.println(s);
+    }
+}
