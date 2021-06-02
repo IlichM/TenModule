f6c4db371a7e9fc5cd0c9cf646c0c2993f0ba88a
IlichM <viruslm@mail.ru>
2021-06-02 23:19:07 +0300

    3 commit

---
+++ src/Massiv/mass1.java
@@ -0,0 +1,22 @@
+package Massiv;
+import java.util.Arrays;
+
+public class mass1 {
+    public static void main(String[] args) {
+        char[] charArray = new char[5];
+        charArray[0] = 'r';
+        charArray[1] = 'a';
+        charArray[2] = 'w';
+        charArray[3] = 'c';
+        charArray[4] = 'h';
+        for(int i = 0; i < charArray.length; i++ ) {
+        System.out.print(charArray[i]); }
+        System.out.println();
+
+        Arrays.sort(charArray);
+        System.out.println(charArray);
+
+        Arrays.fill(charArray, 'f');
+        System.out.println(charArray);
+    }
+}
